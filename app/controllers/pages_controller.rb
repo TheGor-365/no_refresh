02addaf517731483944b0789a454cfbf9f199c43
Gor <7495500@gmail.com>
2023-01-21 13:47:29 +0300

    Add turbo_stream

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,20 @@
+class PagesController < ApplicationController
+  before_action :authenticate_user!
+
+  def home
+  end
+
+  def update_counter
+    current_user.counter += 1
+    current_user.save
+
+    update_counter_text
+  end
+
+  private
+
+  def update_counter_text
+    render turbo_stream:
+      turbo_stream.replace("counter", partial: "pages/counter", locals: { counter: current_user.counter })
+  end
+end
